8b4729cd4902ac3f466e68102ba57be7ebc57981
Carlos Navajas <Carlos Navajas@DESKTOP-DJO3KPK>
2022-11-10 15:43:39 -0600

    Agregar archivos de proyecto.

---
+++ L12_CJNR_1128121/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12_CJNR_1128121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] puntos = new int[15];
+            double suma = 0.0, media = 0.0;
+            try
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    Console.WriteLine(" Ingrese una nota: ");
+                    puntos[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (int i = 0; i < 15; i++)
+                {
+                    suma += puntos[i];
+                }
+                Console.WriteLine(" La suma es: " + suma);
+                media = suma / 15;
+                Console.WriteLine("La media es: " + media);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR! Ingrese un numero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
